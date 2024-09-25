61d7013cddda446a22614e379ee03b66b708f927
konstantin7297 <khromykh.kas@mail.ru>
2024-09-25 19:15:41 +0700

    task 19 fix

--- module_1/task_19.py
+++ module_1/task_19.py
@@ -8,23 +8,47 @@
 Ограничьте размер кеша до 100 записей. При превышении этого лимита удаляйте наиболее старые записи (используйте подход FIFO).
 """
 import functools
+import unittest
 from collections import OrderedDict
 
 
-def cache(func, maxsize: int = 100):
+def cache(maxsize: int = 100):
     """ Декоратор кэширования. """
-    cache_list = OrderedDict()
-    
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        if check_cache := cache_list.get(args, None):
-            return check_cache
+    def decorator(func):
+        cache_list = OrderedDict()
 
-        result = func(*args, **kwargs)
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if check_cache := cache_list.get((args, frozenset(kwargs.items()))):
+                return check_cache
 
-        cache_list[args] = result
-        if len(cache_list.keys()) >= maxsize + 1:
-            cache_list.popitem(last=False)
+            result = func(*args, **kwargs)
+            cache_list[(args, frozenset(kwargs.items()))] = result
+            if len(cache_list) >= maxsize + 1:
+                cache_list.popitem(last=False)
 
-        return result
-    return wrapper
+            return result
+        return wrapper
+    return decorator
+
+
+class TestCacheDecorator(unittest.TestCase):
+    def test_with_args(self):
+        """ Работа с args """
+        @cache(maxsize=5)
+        def add(a, b):
+            return a + b
+
+        result1 = add(2, 3)
+        result2 = add(2, 3)
+        self.assertEqual(result1, result2)
+        self.assertEqual(result1, 5)
+
+    def test_with_kwargs(self):
+        """ Работа с kwargs """
+        @cache(maxsize=5)
+        def add(a, b, c=None):
+            return a + b + (c or 0)
+
+        result = add(1, 2, c=3)
+        self.assertEqual(result, 6)
