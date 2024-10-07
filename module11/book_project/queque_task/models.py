2c8bfc6e86f595b4538ba5792eb8f1ba28e449e6
konstantin7297 <khromykh.kas@mail.ru>
2024-10-07 11:24:38 +0700

    task 13-6 fix

--- module11/book_project/queque_task/models.py
+++ module11/book_project/queque_task/models.py
@@ -1,6 +1,4 @@
-from typing import Union
-
-from django.db import models, transaction
+from django.db import models
 
 
 class Task(models.Model):
@@ -18,16 +16,3 @@ class Task(models.Model):
     worker_id = models.IntegerField(null=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    @staticmethod
-    @transaction.atomic
-    def get_pending(worker_id: int) -> Union['Task', None]:
-        """ Отдает задачу в статусе pending, т.е. свободную. """
-        try:
-            task = Task.objects.select_for_update().get(status="pending")
-            task.status = "processing"
-            task.worker_id = worker_id
-            task.save(update_fields=["status", "worker_id"])
-            return task
-        except Task.DoesNotExist:
-            return None
--- module11/book_project/queque_task/views.py
+++ module11/book_project/queque_task/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
@@ -17,16 +18,22 @@ def worker_process(task: Task) -> Task:
         return task
 
 
-@api_view(['GET'])  # Задача 6 из модуля 13.
+@api_view(['GET'])
 @renderer_classes([JSONRenderer])
+@transaction.atomic
 def fetch_task(request: Request, worker_id: int, create_task: bool = True) -> Response:
     """ Функция для отдачи задачи на worker. """
-    try:
-        if create_task:  # Заглушка на создание задачи.
-            Task.objects.get_or_create(task_name="task1", status="pending")
+    if create_task:
+        Task.objects.get_or_create(task_name="task1", status="pending")
 
-        if task := Task.get_pending(worker_id):  # Попытка получить задачу.
-            return Response({"result": str(worker_process(task).status)})
+    pending_tasks = Task.objects.filter(status="pending")
 
-    except Task.DoesNotExist:
+    if pending_tasks.exists():
+        task = pending_tasks.first()
+        task.status = "processing"
+        task.worker_id = worker_id
+        task.save(update_fields=["status", "worker_id"])
+        return Response({"result": str(worker_process(task).status)})
+
+    else:
         return Response({"result": "Нет задач."})
