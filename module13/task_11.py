fda5eee1034b57a5c2e085035126210592b9b857
konstantin7297 <khromykh.kas@mail.ru>
2024-10-07 12:27:02 +0700

    task 13-11 completed

--- module13/task_11.py
+++ module13/task_11.py
@@ -2,13 +2,39 @@
 Задача - Очередь
 Реализуйте класс который реализует следующий протокол:
 """
+import json
 import typing
+
+import redis
 
 
 class RedisQueue(typing.Protocol):
-    def publish(self, msg: dict):
+    redis_conn = redis.Redis(host='localhost', port=6379, db=0)
+    queue = "my_que"
+
+    @classmethod
+    def publish(cls, msg: dict):
         """ Публикация """
+        msg_json = json.dumps(msg)
+        cls.redis_conn.lpush(cls.queue, msg_json)
 
-    def consume(self) -> dict:
+    @classmethod
+    def consume(cls) -> typing.Union[dict, None]:
         """ Потребление """
+        msg_json = cls.redis_conn.rpop(cls.queue)
+        if msg_json is None:
+            return None
+        return json.loads(msg_json)
 
+
+if __name__ == "__main__":
+    RedisQueue.publish({'key': 'value'})
+    RedisQueue.publish({'key2': 'value2'})
+
+    while True:
+        message = RedisQueue.consume()
+
+        if message is None:
+            break
+
+        print(message)
